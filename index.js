3749f9ab08ee618d84bbb57d7c50369b2dbbbf52
bchandos <contact@billchandos.dev>
2021-10-30 23:11:15 +0000

    add some loading animations

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ class DynamicWidget extends HTMLElement {
     const shadow = this.attachShadow({ mode: 'open' });
     const myDiv = document.createElement('div');
     shadow.appendChild(myDiv);
+    const styleSheet = document.createElement('style');
+    styleSheet.innerHTML = `
+      #mask {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 10;
+      }
+
+      @keyframes pulse-opacity {
+        0% {opacity: 0.6;}
+        50% {opacity: 0.3;}
+        100% {opacity: 0.6;}
+      }
+
+      .pulsing {
+        animation-name: pulse-opacity;
+        animation-duration: 2s;
+        animation-iteration-count: infinite;
+      }
+    `
+    shadow.appendChild(styleSheet);
   }
 
   connectedCallback() {
@@ -24,6 +48,7 @@ class DynamicWidget extends HTMLElement {
   }
 
   async _loadContents() {
+    this.loadingAnimation();
     const dwName = this.getAttribute('name');
     if (this.hasAttribute('elem') && this.getAttribute('elem') !== 'div') {
       this.shadowRoot.firstChild.replaceWith(document.createElement(this.getAttribute('elem')));
@@ -39,6 +64,29 @@ class DynamicWidget extends HTMLElement {
     const initContents = await response.json();
     // Parse newBody for <script> tags to add them to the DOM manually...
     root.innerHTML = initContents.newBody;
+    this.clearLoading();
+  }
+
+  loadingAnimation() {
+    // Cover element and block input
+    const root = this.shadowRoot.firstChild;
+    root.style.position = 'relative';
+    root.classList.add('pulsing');
+    const mask = document.createElement('div');
+    mask.id = 'mask';
+    root.appendChild(mask);
+  }
+
+  clearLoading() {
+    // Clear the loading element
+    const root = this.shadowRoot.firstChild;
+    root.style.position = null;
+    root.classList.remove('pulsing');
+    root.style.opacity = 1;
+    const mask = document.getElementById('mask');
+    if (mask) {
+      mask.remove();
+    }
   }
 }
 
